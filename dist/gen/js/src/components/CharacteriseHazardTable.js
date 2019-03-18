a311c77c2779b8e5297b7d32947f2ad05a67ee39
therter <thorsten.herter@cismet.de>
2019-03-18 09:14:01 +0100

    #1 get the table data from the table_state_rest_api

--- dist/gen/js/src/components/CharacteriseHazardTable.js
+++ dist/gen/js/src/components/CharacteriseHazardTable.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import 'react-table/react-table.css';
 import TableComponent from './commons/TableComponent';
 import SimpleLegendComponent from './commons/SimpleLegendComponent';
+import turf from 'turf';
+import Wkt from 'wicket';
 
 //const CharacteriseHazardTable = () => {
 //    return (<img width={1058} height={578} src='../../../../../../modules/custom/map-component/src/img/01-HC-03-Table.png' />);
@@ -12,7 +14,53 @@ import SimpleLegendComponent from './commons/SimpleLegendComponent';
 export default class CharacteriseHazardTable extends React.Component {
   constructor(props) {
     super(props);
-    var d = [{
+    this.request = {
+      "type": "eu-gl:hazard-characterization",
+      "bbox": [4466000.0, 2130000.0, 4491000.0, 2155000.0],
+      "hazards": [{
+        "hazard": "Heat Wave Duration",
+        "baseline_thresholds": [{
+          "name": "low",
+          "lower": "13.47"
+        }, {
+          "name": "medium",
+          "lower": "13.47",
+          "upper": "15.90"
+        }, {
+          "name": "high",
+          "upper": "15.90"
+        }],
+        "future_thresholds": [{
+          "name": "low",
+          "lower": "9.13"
+        }, {
+          "name": "medium",
+          "lower": "9.13",
+          "upper": "16.6"
+        }, {
+          "name": "high",
+          "upper": "16.6"
+        }],
+        "layers": [{
+          "time-period": "2011-2040",
+          "layer_ids": {
+            "baseline_layer_id": "clarity:Tx75p_consecutive_max_EUR-11_ICHEC-EC-EARTH_historical_r12i1p1_SMHI-RCA4_v1_day_19710101-20001231_netcdf3",
+            "rcp26_layer_id": "clarity:Tx75p_consecutive_max_EUR-11_ICHEC-EC-EARTH_rcp26_r12i1p1_SMHI-RCA4_v1_day_20110101-20401231_netcdf3",
+            "rcp45_layer_id": "clarity:Tx75p_consecutive_max_EUR-11_ICHEC-EC-EARTH_rcp45_r12i1p1_SMHI-RCA4_v1_day_20110101-20401231_netcdf3",
+            "rcp85_layer_id": "clarity:Tx75p_consecutive_max_EUR-11_ICHEC-EC-EARTH_rcp85_r12i1p1_SMHI-RCA4_v1_day_20110101-20401231_netcdf3"
+          }
+        }, {
+          "time-period": "2041-2070",
+          "layer_ids": {
+            "baseline_layer_id": "clarity:Tx75p_consecutive_max_EUR-11_ICHEC-EC-EARTH_historical_r12i1p1_SMHI-RCA4_v1_day_19710101-20001231_netcdf3",
+            "rcp26_layer_id": "clarity:Tx75p_consecutive_max_EUR-11_ICHEC-EC-EARTH_rcp26_r12i1p1_SMHI-RCA4_v1_day_20410101-20701231_netcdf3",
+            "rcp45_layer_id": "clarity:Tx75p_consecutive_max_EUR-11_ICHEC-EC-EARTH_rcp45_r12i1p1_SMHI-RCA4_v1_day_20410101-20701231_netcdf3",
+            "rcp85_layer_id": "clarity:Tx75p_consecutive_max_EUR-11_ICHEC-EC-EARTH_rcp85_r12i1p1_SMHI-RCA4_v1_day_20410101-20701231_netcdf3"
+          }
+        }]
+      }]
+    };
+    this.backupData = [{
       hazard: 'Heat Wave Duration',
       baseline: "Medium",
       earlyResponseScenario: "Low",
@@ -42,50 +90,97 @@ export default class CharacteriseHazardTable extends React.Component {
       period: "2041-2070"
     }];
     this.state = {
-      data: d,
-      allData: d,
+      data: [],
+      allData: [],
 
-      columns: [{
-        Header: ' ',
-        columns: [{
-          Header: 'Hazard',
-          accessor: 'hazard'
-        }] }, {
-        Header: 'Current (1971 - 2000)',
-        columns: [{
-          Header: 'Baseline',
-          accessor: 'baseline',
-          Cell: this.rowWithColor
-        }] }, {
-        Header: React.createElement(
-          'div',
-          null,
-          React.createElement(
-            'span',
-            null,
-            'Future Climate (change compared to baseline)'
-          ),
-          React.createElement(
-            'select',
-            { id: 'futureScenario-combo', onChange: this.changeFutureScenario.bind(this), style: { "marginLeft": '10px' } },
-            this.createOptions(d)
-          )
-        ),
-        columns: [{
-          Header: 'RCP 2.6 (Early Response Scenario)',
-          accessor: 'earlyResponseScenario',
-          Cell: this.rowWithColor
-        }, {
-          Header: 'RCP 4.5 (Effective Measures Scenario)',
-          accessor: 'effectiveMeasuresScenario',
-          Cell: this.rowWithColor
-        }, {
-          Header: 'RCP 8.5 (Business as Usual Scenario)',
-          accessor: 'businessAsUsualScenario',
-          Cell: this.rowWithColor
-        }] }],
+      columns: this.getColumns([]),
       legend: [{ name: 'Low: of no concern for the study', color: '#57d500' }, { name: "Medium: will probably affect some types of elements", color: '#ffbf00' }, { name: "High: will most probably affect some types of elements", color: '#ff2e00' }]
     };
+  }
+
+  getColumns(d) {
+    return [{
+      Header: ' ',
+      columns: [{
+        Header: 'Hazard',
+        accessor: 'hazard'
+      }] }, {
+      Header: 'Current (1971 - 2000)',
+      columns: [{
+        Header: 'Baseline',
+        accessor: 'baseline',
+        Cell: this.rowWithColor
+      }] }, {
+      Header: React.createElement(
+        'div',
+        null,
+        React.createElement(
+          'span',
+          null,
+          'Future Climate (change compared to baseline)'
+        ),
+        React.createElement(
+          'select',
+          { id: 'futureScenario-combo', onChange: this.changeFutureScenario.bind(this), style: { "marginLeft": '10px' } },
+          this.createOptions(d)
+        )
+      ),
+      columns: [{
+        Header: 'RCP 2.6 (Early Response Scenario)',
+        accessor: 'earlyResponseScenario',
+        Cell: this.rowWithColor
+      }, {
+        Header: 'RCP 4.5 (Effective Measures Scenario)',
+        accessor: 'effectiveMeasuresScenario',
+        Cell: this.rowWithColor
+      }, {
+        Header: 'RCP 8.5 (Business as Usual Scenario)',
+        accessor: 'businessAsUsualScenario',
+        Cell: this.rowWithColor
+      }] }];
+  }
+
+  initServer(server, id) {
+    this.loadDataFromServer(server, id, this.request);
+  }
+
+  loadDataFromServer(server, id, requestData) {
+    const obj = this;
+
+    fetch(server + '/jsonapi/group/study?filter[id][condition][path]=id&filter[id][condition][operator]=%3D&filter[id][condition][value]=' + id, { credentials: 'include' }).then(resp => resp.json()).then(function (data) {
+      var wktVar = new Wkt.Wkt();
+      if (data.data[0].attributes.field_area != null && data.data[0].attributes.field_area.value != null) {
+        //          wktVar.read(data.data[0].attributes.field_area.value);
+        //          requestData.bbox = obj.getBoundsFromArea(wktVar.toJson());
+        fetch("https://clarity.meteogrid.com/api/request_hazard", { method: 'POST', body: JSON.stringify(requestData), headers: { 'Content-Type': 'application/json' } }).then(resp => resp.json()).then(function (data) {
+          obj.setState({
+            allData: data,
+            data: data,
+            columns: obj.getColumns(data)
+          });
+          obj.changeFutureScenario();
+        }).catch(function (error) {
+          console.log(JSON.stringify(error));
+          obj.setState({
+            allData: obj.backupData,
+            data: obj.backupData,
+            columns: obj.getColumns(data)
+          });
+          obj.changeFutureScenario();
+        });
+      }
+    }).catch(function (error) {
+      console.log(JSON.stringify(error));
+    });
+  }
+
+  getBoundsFromArea(area) {
+    const bboxArray = turf.bbox(area);
+    const corner1 = [bboxArray[1], bboxArray[0]];
+    const corner2 = [bboxArray[3], bboxArray[2]];
+    var bounds = [corner1, corner2];
+
+    return bounds;
   }
 
   createOptions(d) {
@@ -141,7 +236,9 @@ export default class CharacteriseHazardTable extends React.Component {
   }
 
   render() {
+    window.tableCom = this;
     const header = "The following table shows the previously selected hazards, their indices and their current situation for the selected period, as well as their probable occurances for the three future scenarios (early response, effective measures and business as usual).";
+
     return React.createElement(
       'div',
       null,
--- dist/gen/js/src/components/ExposureTable.js
+++ dist/gen/js/src/components/ExposureTable.js
@@ -10,62 +10,88 @@ import TableComponent from './commons/TableComponent';
 export default class ExposureTable extends React.Component {
   constructor(props) {
     super(props);
+    this.request = {
+      "type": "eu-gl:exposure-evaluation",
+      "bbox": [4650000.0, 1950000.0, 4675000.0, 1975000.0],
+      "data": [{
+        "hazard": "HW,PF,FL",
+        "elementAtRisk": "Population",
+        "vulnerabilityClasses": "Age group 0-14",
+        "layer": "Exposure:Population_men15_naples",
+        "unit": "pop/km2"
+      }, {
+        "hazard": "HW,PF,FL",
+        "elementAtRisk": "Population",
+        "vulnerabilityClasses": "Age group 15-64",
+        "layer": "Exposure:Population_15to65_naples",
+        "unit": "pop/km2"
+      }, {
+        "hazard": "HW,PF,FL",
+        "elementAtRisk": "Population",
+        "vulnerabilityClasses": ">65",
+        "layer": "Exposure:Population_mayor65_naples",
+        "unit": "pop/km2"
+      }]
+    };
+
+    this.backupData = [{
+      hazard: 'HW,PF,FL',
+      elementAtRisk: "Population",
+      classes: "Age group 0-14",
+      values: "1.2",
+      unit: "pop/km2"
+    }, {
+      hazard: 'HW,PF,FL',
+      elementAtRisk: "Population",
+      classes: "Age group 15-64",
+      values: "3.2",
+      unit: "pop/km2"
+    }, {
+      hazard: 'HW,PF,FL',
+      elementAtRisk: "Population",
+      classes: ">65",
+      values: "3.5",
+      unit: "pop/km2"
+    }, {
+      hazard: 'PF,FL',
+      elementAtRisk: "Buildings",
+      classes: "Continuous Residential (S.L. > 80%)",
+      values: "2.1",
+      unit: "m3/m2"
+    }, {
+      hazard: 'PF,FL',
+      elementAtRisk: "Buildings",
+      classes: "Med-Hi Density Discontinuous Res. (30% < S.L. < 80%)",
+      values: "3.2",
+      unit: "m3/m2"
+    }, {
+      hazard: 'PF,FL',
+      elementAtRisk: "Buildings",
+      classes: "Low Density Discontinuous Res. (S.L. < 30%)",
+      values: "2.5",
+      unit: "m3/m2"
+    }, {
+      hazard: 'PF,FL',
+      elementAtRisk: "Buildings",
+      classes: "Non Residential",
+      values: "4.3",
+      unit: "m3/m2"
+    }, {
+      hazard: 'PF,FL',
+      elementAtRisk: "Infrastructure",
+      classes: "Roads",
+      values: "3.3",
+      unit: "ml"
+    }, {
+      hazard: 'PF,FL',
+      elementAtRisk: "Infrastructure",
+      classes: "Railways",
+      values: "3.2",
+      unit: "ml"
+    }];
+
     this.state = {
-      data: [{
-        hazard: 'HW,PF,FL',
-        elementAtRisk: "Population",
-        classes: "Age group 0-14",
-        values: "1.2",
-        unit: "pop/km2"
-      }, {
-        hazard: 'HW,PF,FL',
-        elementAtRisk: "Population",
-        classes: "Age group 15-64",
-        values: "3.2",
-        unit: "pop/km2"
-      }, {
-        hazard: 'HW,PF,FL',
-        elementAtRisk: "Population",
-        classes: ">65",
-        values: "3.5",
-        unit: "pop/km2"
-      }, {
-        hazard: 'PF,FL',
-        elementAtRisk: "Buildings",
-        classes: "Continuous Residential (S.L. > 80%)",
-        values: "2.1",
-        unit: "m3/m2"
-      }, {
-        hazard: 'PF,FL',
-        elementAtRisk: "Buildings",
-        classes: "Med-Hi Density Discontinuous Res. (30% < S.L. < 80%)",
-        values: "3.2",
-        unit: "m3/m2"
-      }, {
-        hazard: 'PF,FL',
-        elementAtRisk: "Buildings",
-        classes: "Low Density Discontinuous Res. (S.L. < 30%)",
-        values: "2.5",
-        unit: "m3/m2"
-      }, {
-        hazard: 'PF,FL',
-        elementAtRisk: "Buildings",
-        classes: "Non Residential",
-        values: "4.3",
-        unit: "m3/m2"
-      }, {
-        hazard: 'PF,FL',
-        elementAtRisk: "Infrastructure",
-        classes: "Roads",
-        values: "3.3",
-        unit: "ml"
-      }, {
-        hazard: 'PF,FL',
-        elementAtRisk: "Infrastructure",
-        classes: "Railways",
-        values: "3.2",
-        unit: "ml"
-      }],
+      data: [],
       columns: [{
         Header: 'Hazards',
         id: 'Hazards',
@@ -94,7 +120,29 @@ export default class ExposureTable extends React.Component {
     });
   }
 
+  initServer(server) {
+    this.loadDataFromServer(server, this.request);
+  }
+
+  loadDataFromServer(server, requestData) {
+    const obj = this;
+
+    fetch("https://clarity.meteogrid.com/api/request_exposure", { method: 'POST', body: JSON.stringify(requestData), headers: { 'Content-Type': 'application/json' } }).then(resp => resp.json()).then(function (data) {
+      obj.setState({
+        allData: data,
+        data: data
+      });
+    }).catch(function (error) {
+      console.log(JSON.stringify(error));
+      obj.setState({
+        allData: obj.backupData,
+        data: obj.backupData
+      });
+    });
+  }
+
   render() {
+    window.tableCom = this;
     //      const header = "The following table and the associated chart show the development of different categories' elements for several scenarios. There are always 3 scenarios considered: 1) the current today's rate development, 2) low rate development and 3) high rate development for the selected time period. The values will be used in assessing the vulnarability, risk and impact in the next steps.";
     return React.createElement(
       'div',
